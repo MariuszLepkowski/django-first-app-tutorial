2370f90c031d7c333497da718e38d7dd544c0417
Mariusz <mariusz.lepkowski23@gmail.com>
2023-11-08 13:06:21 +0100

    Change HOST to db container IP. Add __str__ methods to classes to give models objects a string representation

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": "db",
+        "HOST": os.environ.get('HOST'),
         "PORT": "5432",
     }
 }
