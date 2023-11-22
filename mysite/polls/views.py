cdfc2499d006f892adce85edfcfac7db0de82435
Mariusz <mariusz.lepkowski23@gmail.com>
2023-11-22 18:50:55 +0100

    Refactor vote view to use F() for vote count increment
    By using Django's F() expression, the vote view now safely increments
    the vote count for a selected choice directly at the database level.
    This addresses potential race conditions when multiple users vote
    simultaneously. The change enhances data integrity and ensures atomic
    increments, preventing unexpected behavior during concurrent updates.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -58,7 +59,7 @@ def vote(request, question_id):
             },
         )
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
