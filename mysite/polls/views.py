5b7d96a85146bfb54fcb270b35fe98a6b20c431a
Mariusz <mariusz.lepkowski23@gmail.com>
2023-12-02 17:36:27 +0100

    Modify IndexView to use get_context_data for three latest questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,7 +16,11 @@ from .models import Question, Choice
 class IndexView(ListView):
     model = Question
     template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_question_list'] = Question.objects.order_by("-pub_date")[:3]
+        return context
 
 
 def show_all_questions(request):
